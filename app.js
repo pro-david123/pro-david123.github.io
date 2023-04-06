626fd5b186854589a0523eda174eac5b804e14e6
pro-david123 <128751284+pro-david123@users.noreply.github.com>
2023-04-06 09:20:17 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// Get the refresh button
+var refreshBtn = document.getElementById("refresh-btn");
+
+// Add event listener to refresh button
+refreshBtn.addEventListener("click", function() {
+  window.location.href = './index.html';
+});
