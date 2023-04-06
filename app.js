2bc962782fcee67faca80492889416c65d553cf2
pro-david123 <128751284+pro-david123@users.noreply.github.com>
2023-04-06 21:09:33 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// Get the refresh button
-var refreshBtn = document.getElementById("refresh-btn");
-
-// Add event listener to refresh button
-refreshBtn.addEventListener("click", function() {
-  window.location.href = './index.html';
-});
